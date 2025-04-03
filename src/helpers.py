1b14dd15df764a221a16d9ccc537cf501cd56275
Giorgos Vyronos <gvironos@icloud.com>
2025-04-04 00:28:10 +0100

    Added Delimiter method

--- src/helpers.py
+++ src/helpers.py
@@ -19,3 +19,20 @@ def text_node_to_html_node(text_node:TextNode) -> LeafNode:
             return LeafNode("img","",props={"src":text_node.url,"alt":text_node.text})
         else:
             raise Exception("Not a valid text type")
+
+def split_nodes_delimiter(old_nodes:list[TextNode], delimiter:str, text_type:TextType):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        split_phrase = node.text.split(delimiter)
+        if not len(split_phrase)%2!=0:
+            raise Exception(f"Missing matching delimiter {delimiter}")
+        for idx,item in enumerate(split_phrase):
+            if idx%2==0:
+                if item:
+                    new_nodes.append(TextNode(item,TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(item,text_type))
+    return new_nodes
--- src/test_helpers.py
+++ src/test_helpers.py
@@ -1,6 +1,6 @@
 import unittest
 
-from helpers import text_node_to_html_node
+from helpers import split_nodes_delimiter, text_node_to_html_node
 from textnode import TextNode, TextType
 
 
@@ -52,3 +52,39 @@ class TestHelpers(unittest.TestCase):
         self.assertEqual(html_node.value, "")
         self.assertEqual(html_node.props, {"src":node.url,"alt":node.text})
         self.assertEqual(html_node.to_html(), f'<img src="{node.url}" alt="{node.text}"></img>')
+
+    def test_delimiter_1(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        result = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes,result)
+
+    def test_delimiter_2(self):
+        node = TextNode("*bold block* word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.BOLD)
+        result = [
+            TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes,result)
+
+    def test_delimiter_3(self):
+        node = TextNode("This is a _italic block_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        result = [
+            TextNode("This is a ", TextType.TEXT),
+            TextNode("italic block", TextType.ITALIC),
+        ]
+        self.assertEqual(new_nodes,result)
+
+    def test_delimiter_4(self):
+        node = TextNode("This is a _italic block_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
+        result = [
+            TextNode("This is a _italic block_", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes,result)
