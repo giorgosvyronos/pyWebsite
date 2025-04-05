996297791412af66ddd5883e6a21f8121c7402b6
Giorgos Vyronos <gvironos@icloud.com>
2025-04-05 19:30:30 +0100

    Added file creation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,29 @@
-from textnode import TextNode, TextType
+import os,shutil
 
-textNode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-print(textNode)
+def copy_dir(parent_path=".",new_parent="."):
+    for item in os.listdir(f"{parent_path}"):
+        if not os.path.isfile(f"{parent_path}/{item}"):
+            os.mkdir(f"{new_parent}/{item}")
+            copy_dir(f"{parent_path}/{item}",f"{new_parent}/{item}")
+        else:
+            shutil.copy(f"{parent_path}/{item}",f"{new_parent}/{item}")
+            return
+
+
+def copy_static_to_public():
+    if not ".gitignore" in os.listdir("."):
+        raise RuntimeError("Not running from root directory of Repository")
+    if os.path.isfile("public"):
+        os.remove("public")
+
+    if os.path.exists("./public/"):
+        try:
+            shutil.rmtree("./public/")
+        except Exception as e:
+            raise Exception(f"Encountered error while trying to remove public/ : {e}")
+    os.makedirs("./public/")
+    
+
+    copy_dir("./static/", "./public/")
+
+copy_static_to_public()
