bf9b9e16ef84d82b9792576bd95218770e742dad
Giorgos Vyronos <gvironos@icloud.com>
2025-04-06 12:50:39 +0100

    Moved public to docs

--- src/generate.py
+++ src/generate.py
@@ -2,8 +2,8 @@ import os
 from blocktype import markdown_to_html_node
 from helpers import extract_title
 
-def generate_page(from_path:str,template_path:str,dest_path:str):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+def generate_page(from_path:str,template_path:str,dest_path:str,base_path:str):
+    print(f"Generating page from {base_path}/{from_path} to {base_path}/{dest_path} using {base_path}/{template_path}")
     with open(from_path,encoding="utf-8") as f:
         from_data = f.read()
         f.close()
@@ -19,24 +19,28 @@ def generate_page(from_path:str,template_path:str,dest_path:str):
     html_result = nodes.to_html()
     template_data = template_data.replace("{{ Title }}",title)
     template_data = template_data.replace("{{ Content }}",html_result)
+    # template_data = template_data.replace("href=\"/",f"href=\"{base_path}/")
+    # template_data = template_data.replace("src=\"/",f"src=\"{base_path}/")
+
 
     with open(dest_path,'w', encoding="utf-8") as f:
         f.write(template_data)
         f.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path,base_path="."):
     for item in os.listdir(dir_path_content):
-        if not os.path.isfile(f"{dir_path_content}/{item}"):
-            os.mkdir(f"{dest_dir_path}/{item}")
+        if not os.path.isfile(f"{base_path}/{dir_path_content}/{item}"):
+            os.mkdir(f"{base_path}/{dest_dir_path}/{item}")
             generate_pages_recursive(
-                    f"{dir_path_content}/{item}",
+                    f"{base_path}/{dir_path_content}/{item}",
                     template_path,
-                    f"{dest_dir_path}/{item}"
+                    f"{base_path}/{dest_dir_path}/{item}"
                     )
         else:
-            if f"{dir_path_content}/{item}".endswith("md"):
+            if f"{base_path}/{dir_path_content}/{item}".endswith("md"):
                 generate_page(
-                        f"{dir_path_content}/{item}",
+                        f"{base_path}/{dir_path_content}/{item}",
                         template_path,
-                        f"{dest_dir_path}/{item}".replace("md","html")
+                        f"{base_path}/{dest_dir_path}/{item}".replace("md","html"),
+                        base_path
                     )
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-import os,shutil
-from generate import generate_page, generate_pages_recursive
+import os,shutil,sys
+from generate import generate_pages_recursive
 
 def copy_dir(parent_path=".",new_parent="."):
     for item in os.listdir(f"{parent_path}"):
@@ -10,25 +10,24 @@ def copy_dir(parent_path=".",new_parent="."):
             shutil.copy(f"{parent_path}/{item}",f"{new_parent}/{item}")
 
 
-def copy_static_to_public():
-    if not ".gitignore" in os.listdir("."):
-        raise RuntimeError("Not running from root directory of Repository")
-    if os.path.isfile("public"):
-        os.remove("public")
-
-    if os.path.exists("./public/"):
-        try:
-            shutil.rmtree("./public/")
-        except Exception as e:
-            raise Exception(f"Encountered error while trying to remove public/ : {e}")
-    os.makedirs("./public/")
-    
-
-    copy_dir("./static/", "./public/")
-
 def main():
-    copy_static_to_public()
-    generate_pages_recursive("content","template.html","public")
+
+    if os.path.isfile("docs"):
+        os.remove("docs")
+
+    if os.path.exists("docs"):
+        try:
+            shutil.rmtree("docs")
+        except Exception as e:
+            raise Exception(f"Encountered error while trying to remove docs/ : {e}")
+    os.makedirs("docs")
+    
+    if len(sys.argv)==1:
+        copy_dir("static", "docs")
+        generate_pages_recursive("content","template.html","docs")
+    else:
+        copy_dir(f"{sys.argv[1]}/static", f"{sys.argv[1]}/docs")
+        generate_pages_recursive(f"{sys.argv[1]}/content",f"{sys.argv[1]}/template.html",f"{sys.argv[1]}/docs",f"{sys.argv[1]}")
 
 if __name__ == "__main__":
     main()
